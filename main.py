43fed0accac0b4684adbccc439c7a3dd4510a735
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-06-13 17:50:55 +0000

    feat: Vote multiple times by default

--- main.py
+++ main.py
@@ -12,23 +12,30 @@ def vote_multiple_times(anticaptcha_service_key=None):
     for email in email_generator():
         vote_once(email, anticaptcha_service_key)
 
-        # TODO: Set how long to wait between votes
-        minimim_wait_time = 60
-        maximum_wait_time = 5 * 60
+        # TODO: Adjust how long to wait between votes
+        minimim_wait_time = 5
+        maximum_wait_time = 5 * 5
         random_wait_time = random.randint(minimim_wait_time, maximum_wait_time)
+
+        print('Sleeping for', minimim_wait_time, '-', maximum_wait_time, 'seconds before voting again...')
+        print()
 
         time.sleep(random_wait_time)
 
+    print('Done voting!')
+
 
 def email_generator():
-    # TODO: Fetch email from source then mark it as used in the source so it's not re-used next time
-    # Source can be a file, google sheet, whatever suits your fancy
+    # TODO: Yield emails from a file or google sheets, then mark it as used in the source so it's not re-used next time
+    # If you're not familier with generators and the yield keyword, here's an explanation
+    # https://www.programiz.com/python-programming/generator#create
 
-    new_email = None
-    yield new_email
+    yield 'some_email+' + str(time.time()).split('.')[0] + '@gmail.com'
+    yield 'some_email+' + str(time.time()).split('.')[0] + '@gmail.com'
 
 
 def vote_once(email, anticaptcha_service_key=None):
+    print('Started voting with email:', email)
     votes_before, votes_after = vote(email, anticaptcha_service_key=anticaptcha_service_key)
 
     print('votes_before:', votes_before)
@@ -38,10 +45,8 @@ def vote_once(email, anticaptcha_service_key=None):
 
 
 if __name__ == '__main__':
-    email = 'some_email+' + str(time.time()).split('.')[0] + '@gmail.com'
-
     if len(sys.argv) == 2:
         API_KEY = sys.argv[1]
-        vote_once(email, API_KEY)
+        vote_multiple_times(API_KEY)
     else:
-        vote_once(email)
+        vote_multiple_times()
